8ba8954df9fbc91c5fdcde43c0c14d7711f5f84e
Pieter Smith <pieter@boesman.nl>
2023-01-31 13:47:54 +0100

    add run task for current cpp file

--- 02.classes.cpp
+++ 02.classes.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <iostream> 
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "hello world!";
+    std::cout << "hello world!" << std::endl;
     return 0;
 }
